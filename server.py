4aaf0a4c7b6f17c81c995369ba1b0d297e8e6a6e
Boonsita Pornin <35695097+boonsitap@users.noreply.github.com>
2018-01-22 23:24:43 +0700

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,13 @@ import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5027
 
+def fac(n):  # factorial function
+  num2 = 1
+  while n >= 1:
+    num2 = num2 * n
+    n = n - 1
+    return num2
+    
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
@@ -11,13 +18,6 @@ while True:
     num, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     num2 = int(num) # change type of variable from string to integer
 
-    def fac(n):  # factorial function
-        num2 = 1
-        while n >= 1:
-            num2 = num2 * n
-            n = n - 1
-        return num2
-    
     print "The Answer is ", fac(num2)
 
  
